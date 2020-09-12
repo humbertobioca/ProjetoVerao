52b6d62756e06e0a7d7fefc6b5774637704e50fd
Humberto Bioca <humbertobioca@hotmail.com>
2020-09-12 09:37:37 -0300

    Initial Commit

---
+++ ProjetoVerao/DML/Daily.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ProjetoVerao.DML
+{
+    public class Daily
+    {
+        public int CodigoDaily { get; set; }
+        public string Nome { get; set; }
+        public string Extensao { get; set; }
+        public string Caminho { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataManutencao { get; set; }
+    }
+}
